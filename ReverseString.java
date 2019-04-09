ad123ebfe81968fd06befb7604024ed27400a5c2
alireza <alirezateimoori80@gmail.com>
2019-04-09 17:35:13 -0400

    feat: file created

---
+++ ReverseString.java
@@ -0,0 +1,34 @@
+
+//---------------------------------------
+//-- Created by:     Alireza Teimoori  --
+//-- Created on:     Apr 09 2019       --
+//-- Created for:    Unit 3-01         --
+//-- Course Code:    ICS4U             --
+//-- Teacher Name:   Chris Atkinson    --
+//---------------------------------------
+//-- This program takes a string and   --
+//-- gives it back the reverse         --
+//---------------------------------------
+
+
+
+public class ReverseString {
+
+    // Intro variables:
+    private String userInputString;
+    private String reversedString;
+    private String temp;
+
+    // Constructor:
+    public ReverseString(String userInpuString) { 
+        
+        this.userInputString = userInpuString;
+    }
+
+    // reverseString() function gets a string and returns the reverse
+    public String reverseString() {
+        
+
+        return reversedString;
+    }
+}
