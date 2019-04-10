4cc394a3c5d6ec53c00f35fe963660a8d5f30bc2
alireza <alirezateimoori80@gmail.com>
2019-04-10 01:26:40 -0400

    fix: changed stuff but does not work!

--- ReverseString.java
+++ ReverseString.java
@@ -10,14 +10,16 @@
 //-- gives it back the reverse         --
 //---------------------------------------
 
-
+import java.util.Scanner;
 
 public class ReverseString {
 
     // Intro variables:
     private String userInputString;
-    private String reversedString;
-    private String temp;
+    private String reversedString = "";
+    //private String temp;
+    
+    public static Scanner scanner = new Scanner(System.in);
 
     // Constructor:
     public ReverseString(String userInpuString) { 
@@ -28,7 +30,38 @@ public class ReverseString {
     // reverseString() function gets a string and returns the reverse
     public String reverseString() {
         
+        String thisString = this.userInputString;
+        String revString  = this.reversedString;
 
-        return reversedString;
+        if ( thisString.length() > 1) {
+
+            revString += thisString.charAt( thisString.length() - 1 );
+            setRevString(revString);
+            reverseString();
+
+        } else {
+
+            if ( thisString.length() == 0) { setRevString("Bad!"); }
+            else if ( thisString.length() == 1) { setRevString(revString);; }
+
+        }
+
+
+
+        return this.reversedString;
+    }
+    
+    // setRevString() sets the reversedString field:
+    public void setRevString(String revString) {
+        
+        this.reversedString = revString;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello and welcome! Please enter your String:\n");
+        String userInput = scanner.nextLine();
+        ReverseString reverseString = new ReverseString(userInput);
+        reverseString.reverseString();
     }
 }
